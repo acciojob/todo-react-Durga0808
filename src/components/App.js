4c8304e87de1204bca01b67be6ec0b7011a26836
Durga0808 <bejawada.durgaprasad@gmail.com>
2024-06-01 21:37:46 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,42 @@
 
-import React from "react";
+import React,{ useState } from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((task, i) => i !== index);
+    setTasks(updatedTasks);
+  };
   return (
-    <div>
+    <div className="container">
         {/* Do not remove the main div */}
+        <h1>To-Do List</h1>
+      <input 
+        type="text" 
+        value={newTask} 
+        onChange={(e) => setNewTask(e.target.value)} 
+        placeholder="Enter a new task" 
+      />
+      <button onClick={addTask}>Add Todo</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button className="deleteButton" onClick={() => deleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
